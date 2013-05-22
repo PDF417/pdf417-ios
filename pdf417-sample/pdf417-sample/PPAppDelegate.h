1509260235c45dc8bdb22af55ddccc5289222dd3
jcerovec <jcerovec@gmail.com>
2013-05-22 16:58:41 +0200

    [Sample] Inital project for sample app

---
+++ pdf417-sample/pdf417-sample/PPAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PPAppDelegate.h
+//  pdf417-sample
+//
+//  Created by Jurica Cerovec on 5/22/13.
+//  Copyright (c) 2013 PhotoPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PPViewController;
+
+@interface PPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) PPViewController *viewController;
+
+@end
---
+++ pdf417-sample/pdf417-sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  pdf417-sample
+//
+//  Created by Jurica Cerovec on 5/22/13.
+//  Copyright (c) 2013 PhotoPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PPAppDelegate class]));
+    }
+}

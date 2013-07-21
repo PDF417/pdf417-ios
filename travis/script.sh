e5abde61ea52915bd3caadcf559df94742758f63
jcerovec <jcerovec@gmail.com>
2013-07-22 01:44:45 +0200

    [Travis] Disabling "only active archs" in build phase

--- travis/script.sh
+++ travis/script.sh
@@ -5,22 +5,26 @@ xctool -project pdf417-sample/pdf417-sample.xcodeproj \
 	-scheme pdf417-sample \
 	-configuration Debug \
 	-sdk iphoneos \
+	ONLY_ACTIVE_ARCH=NO \
  	build
 
 xctool -project pdf417-sample/pdf417-sample.xcodeproj \
 	-scheme pdf417-sample \
 	-configuration Release \
 	-sdk iphoneos \
+	ONLY_ACTIVE_ARCH=NO \
  	build
 
 xctool -project pdf417-sample/pdf417-sample.xcodeproj \
 	-scheme pdf417-sample \
 	-configuration Debug \
 	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
  	build
 
 xctool -project pdf417-sample/pdf417-sample.xcodeproj \
 	-scheme pdf417-sample \
 	-configuration Release \
 	-sdk iphonesimulator \
+	ONLY_ACTIVE_ARCH=NO \
  	build

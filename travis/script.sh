93a62cb9046b14e9552c9ba4604b61e16952c850
jcerovec <jcerovec@gmail.com>
2013-07-22 15:38:37 +0200

    [Sample] deployment target change, code sign definition change
    [Travis] Added clean targets in testing script

--- travis/script.sh
+++ travis/script.sh
@@ -4,27 +4,13 @@ set -e
 xctool -project pdf417-sample/pdf417-sample.xcodeproj \
 	-scheme pdf417-sample \
 	-configuration Debug \
-	-sdk iphoneos \
-	ONLY_ACTIVE_ARCH=NO \
- 	build
-
-xctool -project pdf417-sample/pdf417-sample.xcodeproj \
-	-scheme pdf417-sample \
-	-configuration Release \
-	-sdk iphoneos \
-	ONLY_ACTIVE_ARCH=NO \
- 	build
-
-xctool -project pdf417-sample/pdf417-sample.xcodeproj \
-	-scheme pdf417-sample \
-	-configuration Debug \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
- 	build
+ 	clean build
 
 xctool -project pdf417-sample/pdf417-sample.xcodeproj \
 	-scheme pdf417-sample \
 	-configuration Release \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
- 	build
+ 	clean build

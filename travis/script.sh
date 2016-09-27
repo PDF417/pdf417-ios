b8ad4c2e8fb773c10fd23d33259de258204606b1
dino.gustin <dino.gustin@microblink.com>
2016-09-27 13:31:18 +0200

    Updated travis for xcode 8

--- travis/script.sh
+++ travis/script.sh
@@ -1,42 +1,42 @@
 #!/bin/sh
 set -e
 
-xctool -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
+xcodebuild -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
 	-scheme pdf417-sample \
 	-configuration Debug \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
+xcodebuild -project Samples/pdf417-sample/pdf417-sample.xcodeproj \
 	-scheme pdf417-sample \
 	-configuration Release \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/DirectAPI-sample/DirectAPI-sample.xcodeproj \
+xcodebuild -project Samples/DirectAPI-sample/DirectAPI-sample.xcodeproj \
 	-scheme DirectAPI-Sample \
 	-configuration Debug \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/DirectAPI-sample/DirectAPI-sample.xcodeproj \
+xcodebuild -project Samples/DirectAPI-sample/DirectAPI-sample.xcodeproj \
 	-scheme DirectAPI-Sample \
 	-configuration Release \
 	-sdk iphonesimulator \
 	ONLY_ACTIVE_ARCH=NO \
  	clean build
 
-xctool -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
+xcodebuild -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
 -scheme pdf417-sample-Swift \
 -configuration Debug \
 -sdk iphonesimulator \
 ONLY_ACTIVE_ARCH=NO \
 clean build
 
-xctool -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
+xcodebuild -project Samples/pdf417-sample-Swift/pdf417-sample-Swift.xcodeproj \
 -scheme pdf417-sample-Swift \
 -configuration Release \
 -sdk iphonesimulator \

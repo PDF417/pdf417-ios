6a8713177d2e4a77b14bb5100de62399e9f494fa
Dominik Cubelic <dominik@Dominiks-MacBook-Pro.local>
2018-07-11 13:06:10 +0200

    Changed license names to license.txt

--- Samples/pdf417-sample-Swift/pdf417-sample-Swift/ViewController.swift
+++ Samples/pdf417-sample-Swift/pdf417-sample-Swift/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, MBCustomOverlayViewControllerDelegate, M
     override func viewDidLoad() {
         super.viewDidLoad()
         // Valid until: 2019-07-10
-        MBMicroblinkSDK.sharedInstance().setLicenseResource("license-pdf-swift", withExtension: "txt", inSubdirectory: "", for: Bundle.main)
+        MBMicroblinkSDK.sharedInstance().setLicenseResource("license", withExtension: "txt", inSubdirectory: "", for: Bundle.main)
     }
     
     func createScanSettings() -> MBSettings {
